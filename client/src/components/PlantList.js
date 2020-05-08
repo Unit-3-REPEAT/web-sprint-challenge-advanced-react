5cd98948f187c61baf0c92e91ee1d9af8a18be8b
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-05-08 10:32:15 -0500

    installed the dependecies, got the Api server and app server working and set the state to plants

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -4,9 +4,30 @@ import axios from "axios";
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
 
+  constructor(){
+    super();
+    this.state = {
+      plants:[]
+    }
+  }
+
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount(){
+    this.getPlants();
+  }
+
+  getPlants = () => {
+    axios
+    .get('http://localhost:3333/plants')
+    .then(response => {
+      // console.log(response.data.plantsData)
+      this.setState({plants: response.data.plantsData})
+    })
+    .catch(err => console.log(`There was an error fetching plants`, err))
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
